6b7e288c6e241e6ebea8d1f7e93b224415001289
davidpetri <petridw@gmail.com>
2015-11-06 12:16:44 -0800

    (feat) Dev and Prod configs.

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var SlackBot = require('slackbots');
 var getConfig = require('./util/getConfig');
 
-var config = getConfig('development').slackbot;
+var config = getConfig(process.env.NODE_ENV).slackbot;
 
 var bot = new SlackBot({
   token: config.token,
   name: config.name
+});
+
+bot.on('start', function() {
+  
+  bot.postMessageToUser('davidpetri', 'hi', function(data) {
+    console.log('message reply');
+    console.log(data);
+  });
+});
+
+bot.on('message', function(data) {
+  console.log('on message');
+  console.log(data);
 });
