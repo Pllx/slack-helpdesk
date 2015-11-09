25dfea2ee51c3b2f84975b67da8a3bd88d0f5b0c
Wai Hwa Tan <twaih92@gmail.com>
2015-11-09 14:07:21 -0800

    implemented isWorkDay helper function and chart in index.html

--- util/messageHelpers.js
+++ util/messageHelpers.js
@@ -2,7 +2,12 @@ var messageHelper = {};
 var moment = require('moment');
 
 messageHelper.isWorkDay = function() {
-  return true;
+  // Operating Hours : Mon-Fri 9AM-8PM, Sat 9AM-6PM
+  // false if before 9AM || after 8PM || Saturday && after 6PM || Sunday
+  var date = new Date();
+  var hour = date.getHours();
+  var day = date.getDay();
+  return !(hour < 9 || hour >= 20 || (hour >= 18 && day === 6) || day === 0);
 };
 
 module.exports = messageHelper;
